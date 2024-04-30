580072bc8f60a386073db6d34fd9e415d28994fc
Mkrukov <mcshake1000@gmail.com>
2024-04-30 11:39:38 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #493594] ver 2 refactor

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -55,9 +55,7 @@ public class Tracker {
     public void delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            items[index] = null;
-            int endIndex = size == items.length ? size : size - 1;
-            System.arraycopy(items, index + 1, items, index, endIndex - index);
+            System.arraycopy(items, index + 1, items, index, size - 1 - index);
             items[size - 1] = null;
             size--;
         }
