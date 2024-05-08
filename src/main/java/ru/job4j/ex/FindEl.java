4cff25e2ef4fe717dd69b26b02e9a9700e96589a
Mkrukov <mcshake1000@gmail.com>
2024-05-08 11:43:34 +0300

    0.4. Пользовательские исключения. [#219367] ver 2 refactor

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,12 +2,17 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
-                return i;
+                result = i;
+                break;
             }
         }
-        throw new ElementNotFoundException();
+        if (result == -1) {
+            throw new ElementNotFoundException("Такого элемента нет в массиве.");
+        }
+        return result;
     }
 
     public static void main(String[] args) {
