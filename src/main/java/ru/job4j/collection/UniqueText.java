eb9248278092ac0afc23befcf6a00da35b99076c
Mkrukov <mcshake1000@gmail.com>
2024-07-30 07:45:55 +0300

    2. Идентичные тексты [#212655 #507088] ver 2

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -7,14 +7,13 @@ public class UniqueText {
         boolean result = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
-        HashSet<String> check = new HashSet<>();
-
-        for (String word : origin) {
-            check.add(word);
-        }
-
         if (origin.length != text.length) {
             return false;
+        }
+
+        HashSet<String> check = new HashSet<>();
+        for (String word : origin) {
+            check.add(word);
         }
 
         for (String word : text) {
